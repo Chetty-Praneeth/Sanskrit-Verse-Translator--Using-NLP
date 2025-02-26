efc81eee37cedce91faa8a22225a718f27f01083
Chetty-Praneeth <praneeth.chetty.12345@gmail.com>
2025-02-26 19:36:20 +0530

    minior changes

--- translator.py
+++ translator.py
@@ -2,25 +2,24 @@ from googletrans import Translator
 
 translator = Translator()
 
-def translate(text,dest):
+def translate(text, dest):
+    # If the destination is Hindi, translate directly
     if dest == "hi":
-        # Translating the Sanskrit text to Hindi
+        return translator.translate(text, src='auto', dest='hi').text
+
+    # If the destination is English, translate directly from Sanskrit
+    if dest == "en":
         translation = translator.translate(text, src='auto', dest='hi')
-        translated_text =  translation.text
-    elif dest == "en":
-        #if the destination is english then conert it from sanskrit to hindi and then to english
-        # Translate the Sanskrit text to Hindi
-        translation = translator.translate(text, src='auto', dest='hi')
-        # Translate the Hindi text to English
         hi_to_en_translation = translator.translate(translation.text, src='auto', dest='en')
         translated_text =  hi_to_en_translation.text
-    else:
-        #For translation other than Hindi
-        # Translate the Sanskrit text to Hindi
-        translation = translator.translate(text, src='auto', dest='hi')
-        # Translate the Hindi text to English
-        hi_to_en_translation = translator.translate(translation.text, src='auto', dest='en')
-        # Translate the English text to dest
-        en_to_dest_translation = translator.translate(hi_to_en_translation.text, src='en', dest=dest)
-        translated_text = en_to_dest_translation.text
+
+    # For other languages, do a two-step translation via English
+    intermediate_text = translator.translate(text, src='auto', dest='hi')
+
+    hi_to_en_translation = translator.translate(intermediate_text.text, src='auto', dest='en')
+
+    en_to_dest_translation = translator.translate(hi_to_en_translation.text, src='en', dest=dest)
+
+    translated_text = en_to_dest_translation.text
+
     return translated_text
